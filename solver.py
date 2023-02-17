52cdd1c4ee65f8d4138e9411fb19cc9c65ddd699
Vincent Lin <vinlin24@outlook.com>
2023-02-17 07:04:42 -0800

    Remove helpers from old Ctrl+ACV approach

--- solver.py
+++ solver.py
@@ -13,11 +13,10 @@ into which you Ctrl+A and Ctrl+C Ctrl+V the contents of the webpage
 after running the solver.
 """
 
-import re
 import sys
 from dataclasses import dataclass
 from html.parser import HTMLParser
-from typing import List, Literal, Optional, Set, Tuple
+from typing import List, Literal, Optional, Tuple
 
 __author__ = "Vincent Lin"
 
@@ -106,101 +105,6 @@ class ProcessTimes:
     waiting_time: int
 
 
-def is_round_robin(raw_text: str) -> bool:
-    """
-    Return whether the text shows that "Round-Robin, RR" was selected as
-    the algorithm.
-    """
-    match = re.search(r"^Round-Robin, RR$", raw_text, re.MULTILINE)
-    return match is not None
-
-
-def get_num_processes(raw_text: str) -> int:
-    """Extract the number of entries from copy-pasted text."""
-    # Example line:
-    # Average	5587 / 30 = 186.233	5284 / 30 = 176.133
-    match = re.search(r"^Average\s+\d+ / (\d+).+$", raw_text, re.MULTILINE)
-    if match is None:
-        raise ValueError("Could not find Average table entry in string.")
-    return int(match.group(1))
-
-
-def extract_gantt_and_table(raw_text: str) -> Tuple[str, str]:
-    """
-    Parse the raw text gotten from copy-and-pasting the result of Ctrl+A
-    selecting the entire web application after computing the solution
-    for a given process set.  Return the segments corresponding to the
-    Gantt Chart and output table respectively.
-    """
-    # Discard everything before and including the "Gantt Chart" <h2>
-    match = re.search(r"^Gantt Chart$", raw_text, re.MULTILINE)
-    if match is None:
-        raise ValueError("Could not find Gantt Chart header in string.")
-    raw_text = raw_text[match.end()+1:]
-
-    # Discard everthing after and including the "Average" <td>
-    match = re.search(r"^Average.+$", raw_text, re.MULTILINE)
-    if match is None:
-        raise ValueError("Could not find Average table entry in string")
-    raw_text = raw_text[:match.start()]
-
-    # Find the table header within the input to serve as the divider
-    match = re.search(r"^Job.+$", raw_text, re.MULTILINE)
-    if match is None:
-        raise ValueError("Could not find table header in string.")
-
-    # Separate the Gantt Chart and table part of the input
-    header_start, header_end = match.span()
-    gantt_chart = raw_text[:header_start]
-    table_string = raw_text[header_end+1:]
-
-    return (gantt_chart, table_string)
-
-
-def parse_gantt_chart(gantt_chart: str, num_entries: int
-                      ) -> Tuple[List[str], List[int]]:
-    gantt_tokens = gantt_chart.splitlines()
-
-    pids: List[str] = []
-    times: List[int] = []
-
-    # The PIDs become numbers starting from 10, 11, ... after Z
-    numeric_pids: Set[str] = set()
-    for offset in range(num_entries - 26):
-        numeric_pids.add(str(10 + offset))
-
-    # Determine which token is a PID and which is a time
-    for token in gantt_tokens:
-        # Ambiguous, some tricks are needed
-
-        if token in numeric_pids:
-            # If we're already past its value in time, then this token
-            # can't be a time.
-            if times and times[-1] >= int(token):
-                pids.append(token)
-
-        # Obvious cases
-
-        # The "PID" is "_" for slots where no process is executing
-        elif token == "_":
-            # TODO: Somehow discard the start time for _...
-            pass
-        elif token.isalpha():
-            pids.append(token)
-        elif token.isnumeric():
-            time = int(token)
-            # Times can repeat if the chart wraps around, but we need to
-            # make sure there are no duplicates.
-            if not times or times[-1] != time:
-                times.append(time)
-        else:
-            raise ValueError(
-                f"Invalid token found while parsing Gantt Chart: {token!r}.")
-
-
-    return (pids, times)
-
-
 def get_process_times(pids: List[str], times: List[int], table_string: str
                       ) -> Tuple[ProcessTimes]:
     """
@@ -250,16 +154,17 @@ def main() -> None:
 
     filename = sys.argv[1]
     with open(filename, "rt", encoding="utf-8") as fp:
-        raw_text = fp.read()
+        raw_html = fp.read()
 
-    if not is_round_robin(raw_text):
-        sys.stderr.write(
-            "It looks like you didn't select the Round-Robin algorithm!\n")
-        sys.exit(1)
+    chart_parser = GanttChartParser()
+    chart_parser.feed(raw_html)
+    pids = chart_parser.get_pids()
+    times = chart_parser.get_times()
 
-    gantt_chart, table_string = extract_gantt_and_table(raw_text)
-    num_entries = get_num_processes(raw_text)
-    pids, times = parse_gantt_chart(gantt_chart, num_entries)
+    print(pids)
+    print(times)
+
+    return  # TODO.
     process_times = get_process_times(pids, times, table_string)
     size = len(process_times)
 
